612d68d6937114e7e365af100c06466c9516f201
YBO <yitzhak851@gmail.com>
2023-08-10 12:32:48 +0300

    adding more exersize

---
+++ 2- Boot-camp-RUNI/class-3/Ex4/AssignmentTwo.java
@@ -0,0 +1,31 @@
+// Given a number (integer): 
+// •	If the number is outside of the range 0-100 inclusive, print "WOW".
+// •	Otherwise, if it is a power of 10, print "SUCH POWER".
+// •	Otherwise, if is a multiple of 7, print "MUCH LUCK".
+// •	Otherwise, print "VERY CODE".
+
+// For example, for the number 49 the program will print “MUCH LUCK”.
+// For the number 101 the program will print “WOW”.
+
+import java.util.Scanner;
+
+public class AssignmentTwo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int number = scanner.nextInt();
+
+        // Check if the number is outside of the range 0-100 inclusive, print "WOW".
+        if (number >= 100 ) {
+            System.out.println("WOW!!");
+        } else if {
+            // •	Otherwise, if it is a power of 10, print "SUCH POWER".
+
+        }
+
+
+
+
+        scanner.close();
+    }
+}
