e8e4b6fe6d83c8820a4fa14d184439894dfb0e75
YBO <yitzhak851@gmail.com>
2023-08-11 11:01:27 +0300

    commit for backup

--- 2- Boot-camp-RUNI/Recitation1/Ex4/AssignmentTwo.java
+++ 2- Boot-camp-RUNI/Recitation1/Ex4/AssignmentTwo.java
@@ -7,15 +7,15 @@
 // For example, for the number 49 the program will print “MUCH LUCK”.
 // For the number 101 the program will print “WOW”.
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class AssignmentTwo {
     public static void main(String[] args) {
-       // Scanner scanner = new Scanner(System.in);
-        //System.out.print("Enter an integer: ");
-        //int number = scanner.nextInt();
-	int number =233;
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.print("Enter an integer: ");
+        // int number = scanner.nextInt();
 
+        int number = 233;
         // Check if the number is outside of the range 0-100 inclusive, print "WOW".
         if (number >= 100) {
             System.out.println("WOW!!");
@@ -29,6 +29,6 @@ public class AssignmentTwo {
             // • Otherwise, print "VERY CODE".
             System.out.println("VERY CODE!!");
         }
-       // scanner.close();
+        // scanner.close();
     }
 }
---
+++ 2- Boot-camp-RUNI/class-3/DoWhile.java
@@ -0,0 +1,5 @@
+package 2- Boot-camp-RUNI.class-3;
+
+public class DoWhile {
+    
+}
