f86e878de8430b67e7b89acf647ab90bc344a359
YBO <yitzhak851@gmail.com>
2023-08-11 12:37:51 +0300

    regular commit

--- 2- Boot-camp-RUNI/Recitation1/Ex4/AssignmentOne.java
+++ 2- Boot-camp-RUNI/Recitation1/Ex4/AssignmentOne.java
@@ -2,28 +2,20 @@
 // You must use the modulo (%) operator.
 // For example, given the number 99, the program will print 2. Given the number 100, the program will print 3.
 
-import java.util.Scanner;
-
 public class AssignmentOne {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter an integer in the range 0-100: ");
-        int number = scanner.nextInt();
-
+        int number = 233 ;
         // Check if the number is between 0 and 100 (inclusive)
         if (number >= 0 && number <= 100) {
-            // Extract the first and last digits
-            int count = 0; // To store the count of digits
-            // Counting the digits using the modulo operator
-            while (number > 0) {
-                number /= 10; // Remove the last digit
-                count++;
+            if ((number % 100) == 0) {
+                System.out.println("Number of digits: 3 ");
+            } else if (((number % 100) <= 9) && ((number % 100) > 0)) {
+                System.out.println("Number of digits: 1 ");
+            } else if (((number % 100) > 9) ) {
+                System.out.println("Number of digits: 2 ");
             }
-            System.out.println("Number of digits: " + count);
-
         } else {
             System.out.println("The number is not between 0 and 100.");
         }
-        scanner.close();
     }
 }
