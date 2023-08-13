af40be32a512f3a202ec54daef916f9b3deec669
YBO <yitzhak851@gmail.com>
2023-08-13 11:38:53 +0300

    adding casting method

--- 2- Boot-camp-RUNI/class-4/Ex1.java
+++ 2- Boot-camp-RUNI/class-4/Ex1.java
@@ -4,7 +4,14 @@ public class Ex1 {
         String first = args[0];
         String second = args[1];
 
+        int firstInt = Integer.parseInt(first);
+        int secondInt = Integer.parseInt(second);
+        int sum1 = firstInt + secondInt;
+
+
         System.out.println("first input : " + first);
-        System.out.println("first input : " + second );
+        System.out.println("first input : " + second);
+        System.out.println("sum1 input : " + sum1);
+
     }
 }
