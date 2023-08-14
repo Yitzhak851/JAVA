443cc5cec7fec06ea95861909394000355b0c381
YBO <yitzhak851@gmail.com>
2023-08-14 11:29:34 +0300

    adding the for loop excersize

--- 2- Boot-camp-RUNI/Recitation3/Ex1/Assignment31.java
+++ 2- Boot-camp-RUNI/Recitation3/Ex1/Assignment31.java
@@ -1,23 +1,11 @@
-// Use a for loop to print the values 1 to n. 
-// Note: you should set n to be a number of your choice. 
-// (Clarification, n should be set in main, rather than be given as a command-line argument.) 
-// For example, for n=5
-// 1
-// 2
-// 3
-// 4
-// 5
+public class Assignment31 {
+    public static void main(String[] args) {
+        //defined that getting argument from the user
+        int x = Integer.parseInt(args[0]);
 
-
-
-// public class Assignment31 {
-//     public static void main(String[] args) {
-//         int x = Integer.parseInt(args[0]);
-
-//         for(x>0; x++; ){
-
-//         }
-
-
-//     }
-// }
+        //create for loop 
+        for ( int i = 1 ; i <= (x); i++) {
+            System.out.println(i);
+        }
+    }
+}
