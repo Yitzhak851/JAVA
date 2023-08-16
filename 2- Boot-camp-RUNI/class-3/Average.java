b89069c3a6608da907d7a63df38ea4e0fc91d494
YBO <yitzhak851@gmail.com>
2023-08-16 13:09:38 +0300

    daily commit - adding the class 3

--- 2- Boot-camp-RUNI/class-3/Average.java
+++ 2- Boot-camp-RUNI/class-3/Average.java
@@ -1,14 +1,27 @@
 /*
  * Calculates the average of a sequence of given non-negative numbers until -1 is entered
- */ 
+ */
 
 public class Average {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
-        //
-        //
-        //
+        int sum1;
+        int avg1;
+        // TODO- check if (n >= -1)
+        // TRUE - calc the sum from n up to -1
+        // TRUE - calc the avg of the sum/n
+        // FALSE - give num >= -1
+
+        if (n > (-1)) {
+            avg1 = 0; 
+            sum1 = 0;
+            for (int i = 0; i <= n; i++) {
+                sum1 += i;
+            }
+            avg1 = (sum1 / n);
+            System.out.println(" " + avg1);
+        } else {
+            System.out.println(" Give num up from -1 ");
+        }
     }
 }
-
-
