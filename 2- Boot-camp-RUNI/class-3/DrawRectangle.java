39116a7db72b3ccb34c7318aa90304d8641307c8
YBO <yitzhak851@gmail.com>
2023-08-16 13:27:55 +0300

    Adding traingle and rectangle

--- 2- Boot-camp-RUNI/class-3/DrawRectangle.java
+++ 2- Boot-camp-RUNI/class-3/DrawRectangle.java
@@ -5,5 +5,25 @@
  */
 
 public class DrawRectangle {
-    
+    public static void main(String[] args) {
+        // TODO- get from the user by args[0]-width
+        // TODO-get from the user by args[1]-height
+        //TODO- if num ar'not pos - print give pos num
+        // TODO- calc the "*" that print-width
+        // TODO-calc the "*" that print-height
+
+        int x = Integer.parseInt(args[0]); //height
+        int y = Integer.parseInt(args[1]); //width
+        
+        if(((x<= 0) || (y<= 0))){
+            System.out.println("Give positive num");
+        }else{
+            for (int i = 1; i <= y; i++) {
+                for (int j = 1; j <= x; j++) {
+                    System.out.print(" * ");
+                }
+                System.out.println();
+            }
+        }
+    }
 }
--- 2- Boot-camp-RUNI/class-3/DrawTriangle.java
+++ 2- Boot-camp-RUNI/class-3/DrawTriangle.java
@@ -2,9 +2,18 @@
  * Draws a right-angled triangle made of '*' of a given height
  * 
  * NOTE: We assume positive height
+ * 
+ * example:
+ *  1
+ *  1 1
+ *  1 1 1
+ *  1 1 1 1
+ *  
  */
 
 
 public class DrawTriangle {
-    
+    public static void main(String[] args) {
+        //
+    }
 }
