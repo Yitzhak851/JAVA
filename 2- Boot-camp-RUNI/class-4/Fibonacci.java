f4fddb9b9b354ac59f9c3073437dfe822140ee26
YBO <yitzhak851@gmail.com>
2023-08-17 11:46:42 +0300

    Adding fibonachi

--- 2- Boot-camp-RUNI/class-4/Fibonacci.java
+++ 2- Boot-camp-RUNI/class-4/Fibonacci.java
@@ -8,7 +8,28 @@
  * By definition, the first two numbers in the Fibonacci sequence are 1 and 1
  * and each subsequent number is the sum of the previous two.
  */ 
+
+
+//1= 1
+//2= 1
+//3= 1+1
+//4= (1)+(1+1)
+//5= (1+1)+(1+1+1)
+//6= (1+1+1)+(1+1+1+1+1)
+
 class Fibonacci {
   public static void main(String[] args) {
+    int n = Integer.parseInt(args[0]);
+    int previous = 0;
+    int current = 1;
+    
+    //n-> 5
+    for (int i=0; i < n; i++){
+      System.out.println(current);
+      int temp = current;
+      current = current + previous;
+      previous = temp;
+
+    }
   }
 }
--- 2- Boot-camp-RUNI/class-4/IsPrime.java
+++ 2- Boot-camp-RUNI/class-4/IsPrime.java
@@ -3,9 +3,15 @@
  * Reminder: 
  * A prime number is an integer greater than 1 that 
  * has no positive divisors other than 1 and itself
+ * example:  java IsPrime 9 
  */
 public class IsPrime {
-  
   public static void main(String[] args) { 
+    int x = Integer.parseInt(args[0]);
+
+    for( int i = x ; i%   ){
+
+    }
+    
   }
 }
