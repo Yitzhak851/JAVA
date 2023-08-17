d0fd19539ce2acd86b8b14c82b3788f526da6eb1
YBO <yitzhak851@gmail.com>
2023-08-17 10:59:22 +0300

    Adding the first excersize

--- 2- Boot-camp-RUNI/class-4/IsPrime.java
+++ 2- Boot-camp-RUNI/class-4/IsPrime.java
@@ -1,6 +1,5 @@
 /*
  * Checks wether an integer is prime.
- * 
  * Reminder: 
  * A prime number is an integer greater than 1 that 
  * has no positive divisors other than 1 and itself
--- 2- Boot-camp-RUNI/class-4/Power.java
+++ 2- Boot-camp-RUNI/class-4/Power.java
@@ -3,7 +3,14 @@
  * raised to the power of another
  */
 public class Power {
-  
   public static void main(String[] args) { 
+    int x = Integer.parseInt(args[0]);
+    int y = Integer.parseInt(args[1]);
+    int result = 1;
+    //TODO - x^y
+    for(int i = 1; i <= y; i++){
+      result *= x;
+    }
+    System.out.println("The power of " + x + " at "+ y + " is: " + result) ;
   }
 }
