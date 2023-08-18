fa2caba0b32e4a8c50b09bffc5c25595e576d6f0
YBO <yitzhak851@gmail.com>
2023-08-18 14:27:53 +0300

    adding my first  create method at the bootcamp

--- 2- Boot-camp-RUNI/Recitation3/Ex2/Assignment321.java
+++ 2- Boot-camp-RUNI/Recitation3/Ex2/Assignment321.java
@@ -1,30 +1,14 @@
-// // <!-- Assignment 1 -->
-// // Write a Java program that prints to the screen
-// // 10 rows of asterisk characters (*) in ascending order as follows: 
-// // *
-// // **
-// // ***
-// // ****
-// // *****
-// // ******
-// // *******
-// // ********
-// // *********
-// // **********
-
-// // To do so, use a method named printStarRow(...) that receives an integer and prints to the screen 
-// // a corresponding number of asterisk characters. This function should be used 10 times (once per row).
-// // For example, given the input “5”, the expected output of printStarRow(5) is:
-// // *****
-
-// public class Assignment321 {
-//     public static void main(String[] args) {
-//         int n = Integer.parseInt(args[0]);
-
-//         // System.out.println(printStarRow);
-//     // }
-
-//     public void printStarRow(n){
-
-//     }
-// }
+public class Assignment321 {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        for(int i=1; i<=n;i++){
+            printStarRow(i);
+        }   
+    }
+    public static void printStarRow(int x){
+        for(int a = 1; a <= x ; a++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
