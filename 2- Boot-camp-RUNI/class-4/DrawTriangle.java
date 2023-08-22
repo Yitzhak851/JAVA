e16c1e760f078392f20f0953715ae6bc079f7b36
YBO <yitzhak851@gmail.com>
2023-08-22 19:11:54 +0300

    Adding DrawTriangle by for loop

--- 2- Boot-camp-RUNI/class-4/DrawTriangle.java
+++ 2- Boot-camp-RUNI/class-4/DrawTriangle.java
@@ -1,11 +1,21 @@
 /*
  * Draws a right-angled triangle made of '*' of a given height
- * 
  * NOTE: We assume positive height
+ * >>run java DrawTriangle 5
+ * 1- *****
+ * 2- ****
+ * 3- ***
+ * 4- **
+ * 5- *
  */
 public class DrawTriangle {
-	  
-	  public static void main(String[] args) { 
-	    
-	  }
+	public static void main(String[] args) {
+		int h = Integer.parseInt(args[0]);
+		for(int j=h;j>=1;j--){
+			for (int i=1; i<=j; i++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
 }
