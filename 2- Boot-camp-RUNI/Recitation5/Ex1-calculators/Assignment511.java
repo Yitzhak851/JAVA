c4d02c22a936a6512459048b70e1927e370fbdfb
YBO <yitzhak851@gmail.com>
2023-08-23 22:02:03 +0300

    Adding some Ex1 Recitation5 Assignment1

---
+++ 2- Boot-camp-RUNI/Recitation5/Ex1-calculators/Assignment511.java
@@ -0,0 +1,31 @@
+public class Assignment511 {
+    public static double add(double num1, double num2) {
+        double res = num1+num2;
+        return res;
+    }
+    public static double subtract(double num1, double num2){
+        double res = num1-num2;
+        return res;
+    }
+    public static double multiple (double num1, double num2){
+        double res = num1*num2;
+        return res;
+    }
+    public static double divide(double num1, double num2){
+        double res = num1/num2;
+        return res;
+    }
+    public static void main(String[] args) {
+        double a =10;
+        double b =10;
+        System.out.println(add(a,b));
+        System.out.println(subtract(a,b));
+        System.out.println(multiple(a,b));
+        System.out.println(divide(a,b));
+    }
+}
+
+// Write 4 functions that each 
+// take 2 string arguments
+// and does a mathematical operation on
+
