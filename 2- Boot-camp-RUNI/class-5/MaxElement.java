dca593ffcf0e94454cfa5221f1e0aa8147ff3142
YBO <yitzhak851@gmail.com>
2023-08-23 16:28:32 +0300

    Adding the max founction

--- 2- Boot-camp-RUNI/class-5/MaxElement.java
+++ 2- Boot-camp-RUNI/class-5/MaxElement.java
@@ -1,25 +1,21 @@
 public class MaxElement {
-	/*
-	 * recieves an array of integers
-	 * returns the maximal integer found in the array
-	 */
 	public static int findMax(int[] arr) {
-		int max=1;
+		int res =arr[0];
 		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] > arr[i+1]){
-				max=i;
-				return max;
+			if (arr[i] > res){
+				res=arr[i];
 			}
 		}
-		return max;
+		return res;
 	}
 
 	// >> java MaxElement 1 2 3 4 5 (return 5)
 	public static void main(String[] args) {
-		int[] arr = new int[5];
+		int[] arr = new int[args.length];
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = Integer.parseInt(args[i]);
 		}
-		findMax(arr);
+		System.out.println("The max num is: "+ findMax(arr));
+		
 	}
 }
