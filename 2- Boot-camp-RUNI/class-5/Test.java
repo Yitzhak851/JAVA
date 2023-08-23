b5b63e154369b34c4d7d10a12f56d46db4fcb986
YBO <yitzhak851@gmail.com>
2023-08-23 17:00:23 +0300

    commit of the final project

---
+++ 2- Boot-camp-RUNI/class-5/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        int [][] arr = new int [10][10];
+        for(int i=0; i<arr.length; i++){
+
+        }
+        System.out.println();
+    }
+}
