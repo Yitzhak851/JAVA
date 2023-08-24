ed760f532947500146212f35c1e1aa590e2199d1
YBO <yitzhak851@gmail.com>
2023-08-24 17:10:41 +0300

    Adding small commit

---
+++ 2- Boot-camp-RUNI/0-Final-Project/FinalProject.java
@@ -0,0 +1,15 @@
+public class FinalProject {
+    
+}
+
+// <!-- Step 1:  -->
+// Write a function that receives grid dimensions and a probability, 
+// and returns a 2d-array of ints with -1 for every mine, and 0 for every non-mine.
+
+// <!-- Step 2:  -->
+// Write a function that receives a grid of mines, changes every 0 to the
+// number of adjacent mines, and returns it.
+
+// <!-- Step 3:  -->
+// Write a function that receives a coordinate, and returns the value in
+// that coordinate.
