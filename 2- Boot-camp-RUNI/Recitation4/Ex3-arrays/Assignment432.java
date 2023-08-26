ebc73b74059e23afbaba254b138c0244daf9d9c8
YBO <yitzhak851@gmail.com>
2023-08-26 14:32:01 +0300

    update the factorial ecxersize

--- 2- Boot-camp-RUNI/Recitation4/Ex3-arrays/Assignment432.java
+++ 2- Boot-camp-RUNI/Recitation4/Ex3-arrays/Assignment432.java
@@ -1,11 +1,11 @@
 public class Assignment432 {
     public static int[] factorial(int x) { // get 5
-        int[] factorialArray = new int[x+1]; // create factorialArray [5]
+        int[] factorialArray = new int[x+1]; // create factorialArray [n+1]
         System.out.println("0!= " + (factorialArray[0] = 1));
-        int res=1;
+        int likeIndex=1;
         for (int i = 1; i < factorialArray.length; i++) {
-            System.out.println(""+ i + "!= "+(factorialArray[i] = res));
-            res *= ((i) + 1);
+            System.out.println(""+ i + "!= "+(factorialArray[i] = likeIndex));
+            likeIndex *= ((i) + 1);
         }
         return factorialArray;
     }
