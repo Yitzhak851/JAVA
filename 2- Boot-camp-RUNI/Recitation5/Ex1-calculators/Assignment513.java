253bb6c3dc032239404af01ffd184c1c5008f0de
YBO <yitzhak851@gmail.com>
2023-08-26 18:22:47 +0300

    Adding the last version

--- 2- Boot-camp-RUNI/Recitation5/Ex1-calculators/Assignment513.java
+++ 2- Boot-camp-RUNI/Recitation5/Ex1-calculators/Assignment513.java
@@ -1,14 +1,49 @@
-
 public class Assignment513 {
-    
+    public static double doOperation(double num1, String op, double num2){
+        double a=0;
+        if(op == "+"){
+            System.out.println( add(num1, num2));
+        }else if(op == "-"){
+            System.out.println(subtract(num1, num2));
+        }else if(op == "*"){
+            System.out.println(multiple(num1, num2));
+        }else if(op == "/"){
+            System.out.println(divide(num1, num2));
+        }
+        return a;
+    }
+    public static double add(double num1, double num2) {
+        double res = num1+num2;
+        return res;
+    }
+    public static double subtract(double num1, double num2){
+        double res = num1-num2;
+        return res;
+    }
+    public static double multiple (double num1, double num2){
+        double res = num1*num2;
+        return res;
+    }
+    public static double divide(double num1, double num2){
+        double res = num1/num2;
+        return res;
+    }
+    public static void main(String[] args) {
+        String[] arr = new String[args.length];
+
+        String[] arrayOfOperators = new String[(int)(((args.length)-1)/2)];
+        double[] arrayOfNum = new double[(int)(((args.length)/2)+1)];
+        
+        arrayOfOperators = {arr[i+1],arr[i+3], arr[i+5], ..... };
+        arrayOfNum = {arr[i+2], arr[i+4], arr[i+6]...... };
+
+        for(int i=0; i<arrayOfNum.length; i++){
+            doOperation(arrayOfNum[i], null, arrayOfNum[i+1])
+
+        }
+
+
+
+    }
 }
 
-// Write a main function that receives at least three command-line arguments representing an
-// algebraic expression of the form num1 op num2 [op num3 ….] and calculates the result of the
-// expression.
-// For simplicity, evaluate the expression from left to right, ignoring any operating precedence.
-// For example:
-// java Calculator 5 + 5 * 3
-// 30
-// Notice: The return value from above will return 30 as the result and not 20 since we ignore
-// operator precedence.
--- 2- Boot-camp-RUNI/Recitation5/Ex1-calculators/README.md
+++ 2- Boot-camp-RUNI/Recitation5/Ex1-calculators/README.md
@@ -20,12 +20,14 @@ For example, calling: doOperation(5.0, “+”, 2.0) will apply the add function
 in Assignment 1 and will return 7.0 -->
 
 <!-- Assignment 3 -->
-Write a main function that receives at least three command-line arguments representing an
-algebraic expression of the form num1 op num2 [op num3 ….] and calculates the result of the
-expression.
-For simplicity, evaluate the expression from left to right, ignoring any operating precedence.
+Write a main function that receives at least three command-line arguments 
+representing an algebraic expression of the form num1 "op" num2 [op num3 ….] 
+and calculates the result of the expression.
+For simplicity, 
+evaluate the expression from left to right, ignoring any operating precedence.
 For example:
-java Calculator 5 + 5 * 3
+
+>> java Calculator 5 + 5 * 3
 30
 Notice: The return value from above will return 30 as the result and not 20 since we ignore
 operator precedence.
