5cb612d7daebcc8dc488876be9c5fcca1ed6ca90
YBO <yitzhak851@gmail.com>
2023-08-26 22:06:05 +0300

    last commit for today

---
+++ 2- Boot-camp-RUNI/Recitation5/Ex1-calculators/Test.java
@@ -0,0 +1,40 @@
+// package 2- Boot-camp-RUNI.Recitation5.Ex1-calculators;
+
+public class Test {
+    public static void main(String[] args) {
+        if (args.length < 3) {
+            System.out.println("Usage: java Calculator num1 op num2 [op num3 ...]");
+            return;
+        }
+
+        double result = Double.parseDouble(args[0]);
+        String operator = null;
+
+        for (int i = 1; i < args.length; i++) {
+            if (args[i].equals("+") || args[i].equals("-") || args[i].equals("*") || args[i].equals("/")) {
+                operator = args[i];
+            } else {
+                double num = Double.parseDouble(args[i]);
+                if (operator != null) {
+                    switch (operator) {
+                        case "+":
+                            result += num;
+                            break;
+                        case "-":
+                            result -= num;
+                            break;
+                        case "*":
+                            result *= num;
+                            break;
+                        case "/":
+                            result /= num;
+                            break;
+                    }
+                    operator = null;
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+}
