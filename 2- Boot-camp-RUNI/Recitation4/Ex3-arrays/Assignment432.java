f426b00a5042e1386d7f99f06d276ee02c222b57
YBO <yitzhak851@gmail.com>
2023-08-26 14:30:15 +0300

    Adding the factorial Excersize

--- 2- Boot-camp-RUNI/Recitation4/Ex3-arrays/Assignment432.java
+++ 2- Boot-camp-RUNI/Recitation4/Ex3-arrays/Assignment432.java
@@ -1,23 +1,17 @@
-
 public class Assignment432 {
-    public static int[] factorial(int x) {      //get 5
-        
+    public static int[] factorial(int x) { // get 5
+        int[] factorialArray = new int[x+1]; // create factorialArray [5]
+        System.out.println("0!= " + (factorialArray[0] = 1));
+        int res=1;
+        for (int i = 1; i < factorialArray.length; i++) {
+            System.out.println(""+ i + "!= "+(factorialArray[i] = res));
+            res *= ((i) + 1);
+        }
+        return factorialArray;
     }
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
-        System.out.println(factorial(n));
+        System.out.println("The  factorials are: ");
+        factorial(n);
     }
 }
-
-
-// For example, for num=6 the program creates the array:
-// {1, 1, 2, 6, 24, 120}.
-// To create an array of size num, use the following line: int[] factorials = new int [num];
-// Example Output (for num=5)
-// The factorials are:
-// 0! = 1
-// 1! = 1
-// 2! = 2
-// 3! = 6
-// 4! = 24
-// 5! = 120
