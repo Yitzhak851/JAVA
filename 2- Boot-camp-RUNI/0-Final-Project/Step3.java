b593d2c27b7ed2f33bdf9c17efa65d61c5cb0b1d
YBO <yitzhak851@gmail.com>
2023-08-29 13:51:04 +0300

    create step3

---
+++ 2- Boot-camp-RUNI/0-Final-Project/Step3.java
@@ -0,0 +1,12 @@
+
+public class Step3 {
+    public static int[] coordinate(int a, int b) {
+        int[] arr = new int[2];
+        arr[0]=a;
+        arr[1]=b;
+        return arr;
+    }
+}
+// <!-- Step 3:  -->
+// Write a function that receives a coordinate, and returns the value in
+// that coordinate.
