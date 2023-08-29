59f1d76f64ea3c990c1b5aea5545872df0758df0
YBO <yitzhak851@gmail.com>
2023-08-29 09:43:08 +0300

    small change at step 1

--- 2- Boot-camp-RUNI/0-Final-Project/step2.java
+++ 2- Boot-camp-RUNI/0-Final-Project/step2.java
@@ -1,19 +1,16 @@
 public class step2 {
+    //receives grid with mines, changes every 0 to the number of adjacent mines, and returns it.
     public static int[][] updateGrid(int [][] grid) {
-        int[][] gridUpdate = grid;
-
-        for(int i=0; i<gridUpdate.length; i++){
-            for(int j=0; j<gridUpdate[0].length; j++){
-                if(gridUpdate[i][j]!=(-1){
-                    for(int k= i; k<){
-
-                    }
+        int[][] updateGrid = grid;
+        for(int i=0; i<updateGrid.length; i++){
+            for(int j=0; j<updateGrid[i].length; j++){
+                if(updateGrid[i][j]==(-1)){
                 }
             }
         }
+        return updateGrid;
     }
-    // receives the size of the grid nxm, and the probabiility of a mine.
-	// creates a matching random grid
+    // receives size of grid nXm, and probabiility of a mine >> creates a matching random grid.
 	public static int[][] createGrid(int n, int m, double p) {
 		int[][] grid = new int[n][m];                       //f.x: 10X10
 		for (int i = 0; i < grid.length; i++) {
@@ -21,34 +18,30 @@ public class step2 {
 				if (Math.random() < p) {
 					grid[i][j] = -1;
 				}
+                //else:0
 			}
 		}
 		return grid;
 	}
-    	// Receives a grid, and prints it
+    // Receives a grid, and prints it
 	public static void displayGrid(int[][] mineGreed) {
-		for (int row = 0; row < mineGreed.length; row++) {
-			for (int j = 0; j < mineGreed[row].length; j++) {
-				if (mineGreed[row][j] == -1) {
-					System.out.print("x ");
+		for (int i = 0; i < mineGreed.length; i++) {
+			for (int j = 0; j < mineGreed[i].length; j++) {
+				if (mineGreed[i][j] == -1) {
+					System.out.print("-1");
 				} else {
-					System.out.print(mineGreed[row][j] + " ");
+					System.out.print(mineGreed[i][j] + "0");
 				}
 			}
 			System.out.println();
 		}
 	}
     public static void main(String[] args) {
-        int n = Integer.parseInt(args[0]);			// get 3
-		int m = Integer.parseInt(args[1]);			// get 5
-		double p = Double.parseDouble(args[2]);		// get 0.3
+        int n = 10;         //Integer.parseInt(args[0]);			// get 10
+		int m = 10;         //Integer.parseInt(args[1]);			// get 10
+		double p = 0.1;     //Double.parseDouble(args[2]);		    // get 0.1
 		int[][] mineGreed = createGrid(n, m, p);
-        displayGrid(mineGreed);
-        updateGrid(mineGreed);
-
+        displayGrid(mineGreed);  //Receives a grid, and prints it(with mines)
+        displayGrid(updateGrid(mineGreed));  //Receives a grid, and prints it(with mines)
     }
 }
-
-// <!-- Step 2:  -->
-// Write a function that receives a grid of mines, changes every 0 to the
-// number of adjacent mines, and returns it.
