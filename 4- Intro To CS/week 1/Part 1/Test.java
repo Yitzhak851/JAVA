63a71bfd530626348d0bf329ceb74bed3ded0ab1
YBO <yitzhak851@gmail.com>
2023-12-18 18:59:45 +0200

    Adding part 1

--- 4- Intro To CS/week 1/Part 1/Test.java
+++ 4- Intro To CS/week 1/Part 1/Test.java
@@ -1,5 +1,5 @@
 
-public class test {
+public class Test {
         public static void main(String args[]) {
             System.out.println("Congratulations! You've just compiled and executed your first Java program.");
             System.out.println("This indicates that your Java environment seems to be working properly.");
