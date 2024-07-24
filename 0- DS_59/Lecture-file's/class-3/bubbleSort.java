7cea88ae0f370565f729d2878569187668e38708
Yitzhak851 <yitzhak851@gmail.com>
2024-07-24 16:48:38 +0300

    adding refactor class-3

--- 0- DS_59/Lecture-file's/class-3/bubbleSort.java
+++ 0- DS_59/Lecture-file's/class-3/bubbleSort.java
@@ -14,29 +14,21 @@ public class bubbleSort {
     public static void bubS(){
         boolean swapped = true;
         for (int i = 0; i < arr.length - 1; i++) {
-            swapped = false;
+            swapped = false; // statr loop when assume that 2 elements not swapped
             for (int j = 0; j < arr.length - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
                     // Swap arr[j] and arr[j+1]
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
-                    swapped = true;
+                    swapped = true; // after swapped swapped=1
                 }
             }
-            // If no two elements were
-            // swapped by inner loop, then break
-            if (swapped == false)
+            // If no 2 elements were, swapped by inner loop, then break
+            if (swapped == false){
                 break;
+            }
         }
-    }
-
-    public static void swap(int prev, int next, int j1, int j2) {
-        int tmp = next;
-        prev = next;
-        next = tmp;
-        arr[j1] = prev;
-        arr[j2] = next;
     }
 
     public static void printarr() {
@@ -51,4 +43,4 @@ public class bubbleSort {
         System.out.print("]");
     }
 
-}
+} // end of the class
---
+++ 0- DS_59/Recitation-file's/rec-3/README.md
@@ -0,0 +1,1 @@
+<!-- This folder include files from recitation-3 -->
