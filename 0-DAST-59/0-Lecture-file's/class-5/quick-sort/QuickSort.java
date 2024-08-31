d3fe54962043916abb75586f520574356c8d13d4
YBO - Yitzhak Bar Or <93624170+Yitzhak851@users.noreply.github.com>
2024-08-31 18:18:29 +0300

    Update QuickSort.java

--- 0-DAST-59/0-Lecture-file's/class-5/quick-sort/QuickSort.java
+++ 0-DAST-59/0-Lecture-file's/class-5/quick-sort/QuickSort.java
@@ -1,3 +1,8 @@
 public class QuickSort {
+    // merage and concart algoritem
+    // the mainly ide is to choose the correct pivot
+    // and to divide the array by the value of the pivot
+
+    
     
 }
